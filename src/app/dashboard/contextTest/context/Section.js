313e2222deada99fb764f2e8ba49a80b3d56392c
Fanzejie <fanzejiea@gmail.com>
2025-01-14 11:21:30 +0800

    context review

---
+++ src/app/dashboard/contextTest/context/Section.js
@@ -0,0 +1,12 @@
+'use client';
+import { LevelContext } from './LevelContext.js';
+export default function Section({ level,children }) {
+    return (
+      <section className="section">
+        <LevelContext.Provider value={level}>
+        {children}
+      </LevelContext.Provider>
+      </section>
+    );
+  }
+  
